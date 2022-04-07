a2b7a474a6da7adb2d44571a98a551b62ed98500
Adam Haile <adammhaile@gmail.com>
2022-04-06 23:42:54 -0400

    VU firmware for Uno

--- larson_vu/larson_vu_firmware/larson_vu_firmware.ino
+++ larson_vu/larson_vu_firmware/larson_vu_firmware.ino
@@ -1,23 +1,42 @@
-#define USB Serial
-#define Duet Serial1
+#include <FastLED.h>
 
-#define STROBE 16
-#define RESET 17
+#define STROBE 4
+#define RESET 5
 #define DC_One A0
 #define DC_Two A1
 
-// Define spectrum variables
+//globals
 int freq_amp;
-int Frequencies_One[7];
-int Frequencies_Two[7];
-int i;
+int FreqL[7];
+int FreqR[7];
+int SumL = 0;
+int SumR = 0;
+int minv = 2048;
+int maxv = -2048;
 
+// audio clamp values
+#define MIN_LEVEL 60
+#define MAX_LEVEL 600
+#define OUT_MIN 0
+#define OUT_MAX 14
+
+//LED stuff
+#define NUM_LEDS  30
+#define DATA_PIN SPI_DATA
+#define CLOCK_PIN SPI_CLOCK
+CRGB leds[NUM_LEDS];
+
+int led_r_start = (NUM_LEDS / 2);
+int led_l_start = led_r_start - 1;
+
+
+/********************Setup Loop*************************/
 void setup()
 {
-
-    memset(Frequencies_One, 0, sizeof(int) * 7);
-    memset(Frequencies_Two, 0, sizeof(int) * 7);
-
+    FastLED.addLeds<SK9822, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);
+    FastLED.setBrightness(32);
+    
+    // Set spectrum Shield pin configurations
     pinMode(STROBE, OUTPUT);
     pinMode(RESET, OUTPUT);
     pinMode(DC_One, INPUT);
@@ -26,21 +45,97 @@ void setup()
     // Initialize Spectrum Analyzers
     digitalWrite(STROBE, LOW);
     digitalWrite(RESET, LOW);
-    // digitalWrite(DC_One, LOW);
-    // digitalWrite(DC_Two, LOW);
     delay(5);
 
-    USB.begin(57600);
+    Serial.begin(115200);
+    while (!Serial) { delay(1); }
 }
 
-void Read_Frequencies()
+void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
+
+void do_led() { 
+	static uint8_t hue = 0;
+	Serial.print("x");
+	// First slide the led in one direction
+	for(int i = 0; i < NUM_LEDS; i++) {
+		// Set the i'th led to red 
+		leds[i] = CHSV(hue++, 255, 255);
+		// Show the leds
+		FastLED.show(); 
+		// now that we've shown the leds, reset the i'th led to black
+		// leds[i] = CRGB::Black;
+		fadeall();
+		// Wait a little bit before we loop around and do it again
+		delay(10);
+	}
+	Serial.print("x");
+
+	// Now go in the other direction.  
+	for(int i = (NUM_LEDS)-1; i >= 0; i--) {
+		// Set the i'th led to red 
+		leds[i] = CHSV(hue++, 255, 255);
+		// Show the leds
+		FastLED.show();
+		// now that we've shown the leds, reset the i'th led to black
+		// leds[i] = CRGB::Black;
+		fadeall();
+		// Wait a little bit before we loop around and do it again
+		delay(10);
+	}
+}
+
+void do_led_audio()
+{
+    static int x = 0;
+    static int c = 0;
+    
+    FastLED.clear();
+    
+    c = 0;
+    for(x=led_l_start; x>=0; x--)
+    {
+        if(SumL > c)
+        {
+            leds[x] = CRGB(255, 0, 0);
+        }
+        else
+        {
+            break;
+        }
+        c++;
+    }
+    
+    c = 0;
+    for(x=led_r_start; x<NUM_LEDS; x++)
+    {
+        if(SumR > c)
+        {
+            leds[x] = CRGB(255, 0, 0);
+        }
+        else
+        {
+            break;
+        }
+        c++;
+    }
+    
+    FastLED.show();
+}
+
+void loop()
+{
+    ReadAudio();
+    SumChannels();
+    do_led_audio();
+}
+
+void ReadAudio()
 {
     digitalWrite(RESET, HIGH);
     delayMicroseconds(200);
     digitalWrite(RESET, LOW);
     delayMicroseconds(200);
 
-    // Read frequencies for each band
     for (freq_amp = 0; freq_amp < 7; freq_amp++)
     {
         digitalWrite(STROBE, HIGH);
@@ -48,39 +143,45 @@ void Read_Frequencies()
         digitalWrite(STROBE, LOW);
         delayMicroseconds(50);
 
-        Frequencies_One[freq_amp] = analogRead(DC_One);
-        // Frequencies_Two[freq_amp] = analogRead(DC_Two);
+        FreqL[freq_amp] = analogRead(DC_One);
+        FreqR[freq_amp] = analogRead(DC_Two);
     }
 }
 
-void Graph_Frequencies()
+void MapClamp()
 {
+    SumL = map(SumL, MIN_LEVEL, MAX_LEVEL, OUT_MIN, OUT_MAX);
+    SumL = constrain(SumL, OUT_MIN, OUT_MAX);
+    SumR = map(SumR, MIN_LEVEL, MAX_LEVEL, OUT_MIN, OUT_MAX);
+    SumR = constrain(SumR, OUT_MIN, OUT_MAX);
+}
+
+void SumChannels()
+{
+    static int i = 0;
+    SumL = SumR = 0;
     for (i = 0; i < 7; i++)
     {
-        // USB.print((Frequencies_One[i] + Frequencies_Two[i]) / 2);
-        USB.print(Frequencies_One[i]);
-        USB.print("    ");
+        SumL += FreqL[i];
+        SumR += FreqR[i];
     }
-    USB.println();
-}
-
-void loop()
-{
-    Read_Frequencies();
-    Graph_Frequencies();
-    delay(50);
     
-    // digitalWrite(STROBE, LOW);
-    // digitalWrite(RESET, LOW);
-    // digitalWrite(DC_One, LOW);
-    // digitalWrite(DC_Two, LOW);
+    SumL /= 7;
+    SumR /= 7;
     
-    // delay(1000);
+    MapClamp();
     
-    // digitalWrite(STROBE, HIGH);
-    // digitalWrite(RESET, HIGH);
-    // digitalWrite(DC_One, HIGH);
-    // digitalWrite(DC_Two, HIGH);
+    // if(SumL < minv) minv = SumL;
+    // if(SumL > maxv) maxv = SumL;
+    // if(SumR < minv) minv = SumR;
+    // if(SumR > maxv) maxv = SumR;
     
-    // delay(1000);
+    Serial.print(SumL);
+    Serial.print(",");
+    Serial.print(SumR*-1);
+    // Serial.print(",");
+    // Serial.print(minv);
+    // Serial.print(",");
+    // Serial.print(maxv);
+    Serial.println();
 }
