69922c35014766c68395cf4ea746a4937f9370bb
Adam Haile <adammhaile@gmail.com>
2022-04-28 23:44:11 -0400

    tweak gcode scroller run

--- gcode_scroller/run.sh
+++ gcode_scroller/run.sh
@@ -1,5 +1,14 @@
-# add -v to see resulting console char dimensions
+if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+    exit 0
+fi
+
+
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 cd ${SCRIPT_DIR}
+
 export TERM=fbterm
+
+# add -v to see resulting console char dimensions
 fbterm --font-size=16 --font-names="AcPlus IBM BIOS" python3 ./main.py
+
+export TERM=xterm
